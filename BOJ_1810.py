4aa745cf092b000638b185eeb289b9151c0f02d2
LEEHYUNDONG <l.steve10@yahoo.com>
2021-10-05 16:55:37 +0900

    별 찍기

--- BOJ_1810.py
+++ BOJ_1810.py
@@ -9,29 +9,37 @@ plot = [list(map(int, input().split())) for _ in range(n)]
 INF = int(1e9)
 distance = [INF]*(f+1)
 plot.sort(key=lambda x: x[1])
+graph = [[] for _ in range(n)]
 
+# for i in range(len(plot)):
+#     for j in range(i+1, len(plot)):
+#         dist = math.sqrt((plot[i][0]-plot[j][0])**2 +
+#                          (plot[i][1]-plot[j][1])**2)
+#         graph[plot[i][1]].append(dist, plot[j][0])
 
 def dijskstra(x, y):
     q = []
-    heapq.heappush(q, (INF, x, y))
-    distance[y] = INF
+    heapq.heappush(q, (0, x, y))
+    distance[y] = 0
     while q:
         dist, rx, ry = heapq.heappop(q)
-        if dist > distance[ry]:
-            continue
+        # if dist > distance[ry]:
+        #     continue
         for dx, dy in plot:
-            cost = math.sqrt((dx-rx)**2 + (dy-ry)**2)
-            if dx != rx or dy != ry:
+            if abs(dx-rx) <= 2 and abs(dy-ry) <= 2:
+                cost = math.sqrt((dx-rx)**2 + (dy-ry)**2) + dist
+                #if dx != rx or dy != ry:
                 if cost < distance[dy]:
                     distance[dy] = cost
                     heapq.heappush(q, (cost, dx, dy))
-                    print(rx, ry)
-        if ry == f:
-            break
+                    print(dx, dy)
+        # if ry == f:
+        #     break
 
 
 if plot[-1][1] < f:
     print(-1)
 else:
     dijskstra(0, 0)
-    print(math.ceil(sum(distance[1:])))
+    #print(math.ceil(sum(distance[1:])))
+    print(distance)
